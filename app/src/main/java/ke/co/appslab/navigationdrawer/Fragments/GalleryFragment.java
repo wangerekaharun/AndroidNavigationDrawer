3b11504de8ea2000cf8ee2e16240f4a25579d12f
wangerekaharun <harunwangereka@gmail.com>
2018-01-10 14:31:08 +0300

    navigation drawer demo complete

---
+++ app/src/main/java/ke/co/appslab/navigationdrawer/Fragments/GalleryFragment.java
@@ -0,0 +1,23 @@
+package ke.co.appslab.navigationdrawer.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ke.co.appslab.navigationdrawer.R;
+
+/**
+ * Created by harun on 1/10/18.
+ */
+
+public class GalleryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.gallery_fragment, container, false);
+        getActivity().setTitle("Gallery");
+
+        return view;
+    }
+}
